a49b7b7a66b98669cbaa93b00d745032cba596ea
Ted Schelble <44124549+tschelbs18@users.noreply.github.com>
2019-05-20 00:12:45 -0400

    AlgoExpert Largest Range

---
+++ largest_range.py
@@ -0,0 +1,42 @@
+def largestRange(array):
+    # Write your code here.
+	# Sort it, then determine longest continuation?
+	array.sort()
+	max_range_len = 0
+	max_range_start = 0
+	curr_range_len = 0
+	curr_range_start = 0
+	curr_range_bool = False
+	
+	array_set = set(array)
+	array_list = list(array_set)
+	array_list.sort()
+	
+	if len(array_list) == 1:
+		return [array_list[0],array_list[0]]
+	
+	for idx, value in enumerate(array_list):
+		if idx == 0: 
+			pass
+		elif value == array_list[idx - 1] + 1:
+			if curr_range_bool == False:
+				curr_range_bool = True
+				curr_range_start = array_list[idx - 1]
+				curr_range_len = 1
+			else:
+				curr_range_len += 1
+		elif curr_range_bool:
+			curr_range_bool = False
+			if curr_range_len > max_range_len:
+				max_range_len = curr_range_len
+				max_range_start = curr_range_start
+				curr_range_len = 0
+				curr_range_start = 0
+	if curr_range_len > max_range_len:
+		max_range_len = curr_range_len
+		max_range_start = curr_range_start
+	return [max_range_start, max_range_start + max_range_len]
+		
+		
+		
+	
